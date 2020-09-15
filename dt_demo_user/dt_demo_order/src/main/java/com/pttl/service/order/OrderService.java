87c4b9aa61f8c4899157f4d11577d816c8c68d97
srchen1987 <suxuan696@gmail.com>
2020-09-15 22:39:19 +0800

    del projects

--- dt_demo_user/dt_demo_order/src/main/java/com/pttl/service/order/OrderService.java
+++
@@ -1,6 +0,0 @@
-package com.pttl.service.order;
-import com.pttl.service.order.entity.Order;
-public interface OrderService {
-	public int insertOrder(Order order);
-	boolean buyProduct(String product, String num);
-}
--- dt_demo_user/dt_demo_product/src/main/java/com/pttl/service/product/ProductService.java
+++
@@ -1,5 +0,0 @@
-package com.pttl.service.product;
-
-public interface ProductService {
-	public int updateRepertory(String brachTxId,int productid,int repertory);
-}
