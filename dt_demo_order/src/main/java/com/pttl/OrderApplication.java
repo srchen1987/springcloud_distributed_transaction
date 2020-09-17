3583091a20c2cf59c44f111011d74819dbd343b0
srchen1987 <suxuan696@gmail.com>
2020-09-17 15:23:05 +0800

    update pom

--- dt_demo_order/src/main/java/com/pttl/OrderApplication.java
+++ dt_demo_order/src/main/java/com/pttl/OrderApplication.java
@@ -3,10 +3,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.netflix.feign.EnableFeignClients;//1.5x打开这个包
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
